dd2a5afc548b78a6aca77e4ce5400478d337e6cc
kim4848 <kj@kjsoft.dk>
2018-02-21 08:04:09 +0100

    Init

--- RSAencrypt/Program.cs
+++ RSAencrypt/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace RSAencrypt
 {
@@ -10,6 +12,37 @@ namespace RSAencrypt
     {
         static void Main(string[] args)
         {
+            //Only publickey - cant decrypt
+            //X509Certificate2 cer = new X509Certificate2("TestPublicKeyCert.cer", "Test1234", X509KeyStorageFlags.Exportable);
+
+            //with privatkey
+            X509Certificate2 cer = new X509Certificate2("TestPrivatKeyCert.pfx", "Test1234", X509KeyStorageFlags.Exportable);
+            RSAcrypt crypt = new RSAcrypt(cer);
+
+            Console.WriteLine("Type text to be encrypted: \n ");
+            var input =Console.ReadLine();
+            
+            var encryptedInput = crypt.RSAEncrypt(input);
+            Console.WriteLine("\n\nEncrypted: \n" + encryptedInput);            
+            Console.WriteLine("\nEncrypted Base64Encoded: \n" + Base64Encode(encryptedInput));
+
+            try
+            {
+                var decryptedResult = crypt.RSADecrypt(encryptedInput);
+                Console.WriteLine("\nDecrypted: \n" + decryptedResult);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nMessage could not be decrypted : \n");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
         }
     }
 }
---
+++ RSAencrypt/RSA.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace RSAencrypt
+{
+    class RSAcrypt
+    {
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA { get; set; }
+        RSACryptoServiceProvider RSApublic = new RSACryptoServiceProvider();
+
+        public RSAcrypt(X509Certificate2 cert)
+        {
+            if (cert.HasPrivateKey)
+            {
+                RSA = (RSACryptoServiceProvider)cert.PrivateKey;
+            }
+            RSApublic = (RSACryptoServiceProvider)cert.PublicKey.Key;
+        }     
+
+        public string RSAEncrypt(string data)
+        {
+            byte[] plaintext = ByteConverter.GetBytes(data);
+            byte[] encryptedtext = Encryption(plaintext, RSApublic.ExportParameters(false), false);
+            return Encoding.Default.GetString(encryptedtext);
+        }
+
+        public string RSADecrypt(string data)
+        {
+            if (RSA != null)
+            {
+                byte[] decryptedtex = Decryption(Encoding.Default.GetBytes(data), RSA.ExportParameters(true), false);
+                return ByteConverter.GetString(decryptedtex);
+            }
+            else
+            {
+                throw new Exception("Certificate does not contain a private key");
+            }
+        }
+
+        byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+    }
+}
